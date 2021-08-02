c78e5ba99c808318e0789fe6ef2ffcca52ba5886
Govind Saini <quebert88@gmail.com>
2021-08-02 12:56:34 +0530

    govind

---
+++ schemas/objects/homeTrendingOffer.js
@@ -0,0 +1,40 @@
+export default {
+    type: "object",
+    name: "homeTrendingOffer",
+    title: "user Counter ",
+    fields: [
+      {
+        type: "string",
+        name: "bank_Name",
+        title: "Bank Name",
+      },
+   
+      {
+      type: "string",
+      name: "typeOfLoan",
+      title: "Type Of Laon",
+    },
+    {
+        type: "image",
+        name: "bank image",
+        title: "Bank Image",
+      },
+    {
+        type: "string",
+        name: "bankOffer",
+        title: "Bank Offer",
+      },
+      {
+        type: "string",
+        name: "bankOffers",
+        title: "Bank Offers",
+      },
+      {
+        type: "string",
+        name: "typeOfLoanOffer",
+        title: "Bank Loan Offer",
+      },
+    
+    ],
+  };
+  
