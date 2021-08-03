b14d7692ffde7b208e567e8314ffcbbff180b34d
Govind Saini <quebert88@gmail.com>
2021-08-03 11:34:10 +0530

    govind

--- schemas/objects/homeTextWithCard.js
+++ schemas/objects/homeTextWithCard.js
@@ -1,23 +1,22 @@
 export default {
-    type: "object",
-    name: "homeTextWithCard",
-    title: "Home TextWithCard ",
-    fields: [
-      {
-        type: "image",
-        name: "icon",
-        title: "Icon",
-      },
-      {
-        type: "string",
-        name: "TitleName",
-        title: "Title",
-      },
-      {
-        type: "image",
-        name: "icons",
-        title: "Icons",
-      },
-    ],
-  };
-  
+  type: "object",
+  name: "homeTextWithCard",
+  title: "Home TextWithCard ",
+  fields: [
+    {
+      type: "image",
+      name: "icon",
+      title: "Icon",
+    },
+    {
+      type: "string",
+      name: "TitleName",
+      title: "Title",
+    },
+    {
+      type: "string",
+      name: "altImage",
+      title: "Alt Image Card",
+    },
+  ],
+};
