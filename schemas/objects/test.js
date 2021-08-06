9a583405a98ba39eaad9db4561455beb1c7f4e20
Govind Saini <quebert88@gmail.com>
2021-08-06 16:04:40 +0530

    govind

---
+++ schemas/objects/test.js
@@ -0,0 +1,17 @@
+export default {
+  title: "Test",
+  name: "test",
+  type: "object",
+  fields: [
+    {
+      type: "text",
+      title: "Title",
+      name: "title",
+    },
+    {
+      type: "image",
+      title: "Icon",
+      name: "icon",
+    },
+  ],
+};
