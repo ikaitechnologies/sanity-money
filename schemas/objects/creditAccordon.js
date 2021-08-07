5966b0562b25a53f6c79e758de90f5a0b1273356
hariom shah <hariom23shah@gmail.com>
2021-08-07 16:58:04 +0530

    saurav

--- schemas/objects/creditAccordon.js
+++ schemas/objects/creditAccordon.js
@@ -1,18 +1,30 @@
 export default {
-    type: "object",
-    name: "creditAccordon",
-    title: "credit Accordon ",
-    fields: [
-      {
-        type: "string",
-        name: "creditAccordonQuestion",
-        title: "Credit Accordon Question",
-      },
-      {
-        type: "text",
-        name: "creditAccordonAnswer",
-        title: "Credit Accordon Answer",
-      },
-    ],
-  };
-  
+  type: "object",
+  name: "creditAccordon",
+  title: "credit Accordon ",
+  fields: [
+    {
+      type: "string",
+      name: "creditAccordonQuestion",
+      title: "Credit Accordon Question",
+    },
+    {
+      type: "string",
+      name: "creditAccordonAnswer",
+      title: "Credit Accordon Answer",
+    },
+    // {
+    //   type: "array",
+    //   name: "creditAccordonAnswer",
+    //   title: "Credit Accordon Answer",
+    //   of: [
+    //     {
+    //       type: "block",
+    //     },
+    //     {
+    //       type: "table",
+    //     },
+    //   ],
+    // },
+  ],
+};
